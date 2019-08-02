94a91360f4d64b1fcc6db41bd6b52b65c5385df0
Ihar Suvorau <ihar.suvorau@gmail.com>
2019-08-02 12:16:22 +0300

    dynamic coloring fixed

--- resources/main.js
+++ resources/main.js
@@ -93,12 +93,39 @@ function initColorScheme() {
     dataType:'jsonp',
     type: 'get',
     success: function(response){
-      setColorScheme(Date.now() > Date.parse(response['results']['sunset']));
+      // now
+      var now = new Date();
+      var nowHours = now.getHours();
+      var nowMins = now.getMinutes();
+      // sunrise
+      var sunriseArr = parseTime(response['results']['sunrise']);
+      var sunriseHours = sunriseArr[0];
+      var sunriseMins = sunriseArr[1];
+      // sunset
+      var sunsetArr = parseTime(response['results']['sunset']);
+      var sunsetHours = sunsetArr[0];
+      var sunsetMins = sunsetArr[1];
+      // comparison
+      setColorScheme(!(nowHours > sunriseHours && nowHours < sunsetHours && nowMins > sunriseMins && nowMins < sunsetMins))
     },
     error: function(XMLHttpRequest, textStatus, errorThrown) {
       console.log(XMLHttpRequest, textStatus, errorThrown)
     }
   });
+}
+
+// Parses time in the format of '2019-08-02T02:07:07+00:00'. According
+// to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+// Date.parse() is discouraged until ES5.
+function parseTime(datetime) {
+  if (datetime) {
+    var timeAndZone = datetime.split('T')[1]; // in UTC
+    var time = timeAndZone.split('+')[0];
+    var timeArr = time.split(':');
+    var hours = parseInt(timeArr[0]) + 3; // current zone
+    var mins = parseInt(timeArr[1]);
+    return [hours, mins];
+  }  
 }
 
 function setColorScheme(isNight) {
